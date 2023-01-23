703674ee1ce31a4d52386a2a2c130c3952ab46ca
Hirushini Dematagoda <87405106+hirushinidem@users.noreply.github.com>
2023-01-23 03:31:18 +0000

    fetch data from api

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "a1c0bee4cdb843b5bcf41cf521776f3d",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "colombo,lk",
 };
